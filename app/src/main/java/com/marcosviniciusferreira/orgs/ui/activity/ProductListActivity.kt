405c146f1b219a551daede969f60e6d65e2e12c1
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-03-03 15:27:12 -0300

    add recycler view item click listener

--- app/src/main/java/com/marcosviniciusferreira/orgs/ui/activity/ProductListActivity.kt
+++ app/src/main/java/com/marcosviniciusferreira/orgs/ui/activity/ProductListActivity.kt
@@ -2,7 +2,9 @@ package com.marcosviniciusferreira.orgs.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -43,6 +45,11 @@ class ProductListActivity : AppCompatActivity() {
         recyclerView.hasFixedSize()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter.itemClickListener = {
+            
+            Log.i("=====>>> LIST", "itemClickListener ${it.name}")
+            Toast.makeText(this, "itemClickListener ${it.name}", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun settingFab() {
--- app/src/main/java/com/marcosviniciusferreira/orgs/ui/recyclerview/adapter/ProductListAdapter.kt
+++ app/src/main/java/com/marcosviniciusferreira/orgs/ui/recyclerview/adapter/ProductListAdapter.kt
@@ -1,6 +1,7 @@
 package com.marcosviniciusferreira.orgs.ui.recyclerview.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,16 +17,30 @@ import java.util.*
 import java.util.zip.Inflater
 
 class ProductListAdapter(
-    private val context: Context, products: List<Product>
+    private val context: Context,
+    products: List<Product>,
+    var itemClickListener: (product: Product) -> Unit = {}
 
 ) : RecyclerView.Adapter<ProductListAdapter.MyViewHolder>() {
 
     private val products = products.toMutableList()
 
-
-    class MyViewHolder(private val binding: ProductItemBinding) :
+    inner class MyViewHolder(private val binding: ProductItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
+        private lateinit var product: Product
+
+        init {
+            itemView.setOnClickListener{
+                if (::product.isInitialized) {
+                    itemClickListener(product)
+                }
+            }
+        }
+
+
         fun bind(product: Product) {
+            this.product = product
             val name = itemView.findViewById<TextView>(R.id.productName)
             val description = itemView.findViewById<TextView>(R.id.productDescription)
             val price = itemView.findViewById<TextView>(R.id.productPrice)
