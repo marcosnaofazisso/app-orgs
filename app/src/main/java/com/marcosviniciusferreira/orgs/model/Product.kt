320013652c58cfc8c45702bcc8ebe009416ed6dd
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-03-04 09:51:51 -0300

    add room to project and add entity to product model

--- app/src/main/java/com/marcosviniciusferreira/orgs/model/Product.kt
+++ app/src/main/java/com/marcosviniciusferreira/orgs/model/Product.kt
@@ -1,11 +1,18 @@
 package com.marcosviniciusferreira.orgs.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
 
 @Parcelize
+@Entity
 data class Product(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+
     val name: String,
     val description: String,
     val price: BigDecimal,
